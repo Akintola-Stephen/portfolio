eb7f51ff53ee6aab4142f4613019897dbfef2544
AKINTOLA STEPHEN IYANU <40688762+Akintola-Stephen@users.noreply.github.com>
2021-11-11 15:54:50 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,7 +39,7 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed/v1/place?q=5c+Ayo+Babalola+green+valley+Magodo+Lagos&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
@@ -48,7 +48,7 @@ export default function Contact() {
               </h2>
               <p className="mt-1">
                 Lagos, Nigeria. <br />
-                New York, NY 1
+  
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -61,6 +61,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
+              
               <p className="leading-relaxed">(+234)9074158101</p>
             </div>
           </div>
@@ -74,8 +75,8 @@ export default function Contact() {
             Hire Me
           </h2>
           <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            Contact me via this contact me end point form <br/>
+            I'll reach out immediately messge gets received.
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
